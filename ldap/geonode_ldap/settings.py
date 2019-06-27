1bb3ae61d50062297f7644d0eeea86e9556f0f34
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2019-06-27 12:56:59 +0100

    GEONODE_LDAP_GROUP_PROFILE_FILTERSTR now has a default value

--- ldap/geonode_ldap/settings.py
+++ ldap/geonode_ldap/settings.py
@@ -1,12 +1,11 @@
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 
 
-try:
-    LDAP_GROUP_PROFILE_FILTERSTR = settings.GEONODE_LDAP_GROUP_PROFILE_FILTERSTR
-except AttributeError:
-    raise ImproperlyConfigured(
-        "Please set the GEONODE_LDAP_GROUP_PROFILE_FILTERSTR variable")
+LDAP_GROUP_PROFILE_FILTERSTR = getattr(
+    settings,
+    "GEONODE_LDAP_GROUP_PROFILE_FILTERSTR",
+    "(objectClass=*)"
+)
 
 LDAP_GROUP_NAME_ATTRIBUTE = getattr(
     settings,
