e5cadc2c02f407e43e416cb1c8d40f555af30eec
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2022-05-12 10:34:19 +0200

    Revert "Remove SOS library from contribs (#177)"
    This reverts commit d14068882102c562435d40bbaf07321489e49ff4.

---
+++ sos/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+import geonode_sos
+
+
+def read_file(path: str):
+    with open(path, "r") as file:
+        return file.read()
+
+
+setup_requires = [
+    "wheel",
+]
+
+setup(
+    name="geonode_sos",
+    version=geonode_sos.__version__,
+    url=geonode_sos.__url__,
+    description=geonode_sos.__doc__,
+    long_description=read_file("README.md"),
+    author=geonode_sos.__author__,
+    author_email=geonode_sos.__email__,
+    license=geonode_sos.__license__,
+    platforms="any",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django :: 3.0",
+        "License :: OSI Approved :: GNU General Public License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    package_data={"": ["*.html", "*.js"]},
+    include_package_data=True,
+    install_requires=[
+        "sos4py==0.3.0",
+        "django-dynamic-model @ git+https://github.com/mattiagiupponi/django-dynamic-models.git@master#egg=dynamic_models"
+    ],
+)
